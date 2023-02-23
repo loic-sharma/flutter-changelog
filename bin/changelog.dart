5fd262c960bc32bd0429d96ed1a60a08a429e8a3
Loic Sharma <sharma.loic@gmail.com>
2023-02-22 20:24:26 -0800

    Start

---
+++ bin/changelog.dart
@@ -0,0 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart' show DateFormat;
+import 'package:uri/uri.dart' show UriBuilder;
+
+void main(List<String> arguments) {
+  String? token = 'ghp_ABCD1234';
+
+  
+}
---
+++ test/changelog_test.dart
@@ -0,0 +1,8 @@
+import 'package:changelog/changelog.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
