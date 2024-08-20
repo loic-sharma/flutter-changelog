a29c7d40260224ce93b5d1bfcd160c18f03e73c0
Loïc Sharma <737941+loic-sharma@users.noreply.github.com>
2024-08-20 11:14:58 -0700

    Refactor output logic & improve reviewer logic (#2)

--- output.dart
+++ output.dart
@@ -4,247 +4,174 @@ import 'package:intl/intl.dart';
 
 import './github.dart';
 
-typedef ChangelogWriter = Future<void> Function(
-  IOSink readme,
-  IOSink list,
-);
+class ChangelogWriter {
+  ChangelogWriter(
+    this._output, {
+    addBreaks = true,
+  }) : _addBreaks = addBreaks;
 
-typedef UnassignedPullRequestWriter = Future<void> Function(
-  IOSink pullRequests,
-);
+  final IOSink _output;
+  final bool _addBreaks;
 
-Future<void> writeChangelog(ChangelogWriter writer) async {
-  final readme = File('README.md').openWrite();
-  readme.writeln('# Flutter changelog');
-  readme.writeln();
-
-  final list = File('list.md').openWrite();
-  list.writeln('# Flutter changelog');
-  list.writeln();
-
-  await writer(readme, list);
-  await readme.flush();
-  await list.flush();
-
-  readme.close();
-  list.close();
-}
-
-Future<void> writeUnassignedPullRequests(UnassignedPullRequestWriter writer) async {
-  final pulls = File('pulls.md').openWrite();
-  pulls.writeln('# Flutter pull requests without reviewers');
-  pulls.writeln();
-
-  await writer(pulls);
-  await pulls.flush();
-  pulls.close();
-}
-
-void writeCommitsList(
-  IOSink output,
-  String owner,
-  String repository,
-  Map<String, List<Commit>> commits, {
-    bool addBreaks = true,
+  void writeHeader() {
+    _output.writeln('# Flutter changelog');
+    _output.writeln();
   }
-) {
-  final htmlBreak = addBreaks ? '<br />' : '';
 
-  output.writeln('## $owner/$repository');
-  output.writeln();
+  void writeCommitsList(
+    String owner,
+    String repository,
+    Map<String, List<Commit>> commits,
+  ) {
+    final htmlBreak = _addBreaks ? '<br />' : '';
 
-  var first = true;
-  for (final subsection in commits.entries) {
-    output.writeln('### ${subsection.key}');
-    output.writeln();
+    _output.writeln('## $owner/$repository');
+    _output.writeln();
 
-    if (first) {
-      output.writeln('${subsection.value.length} commits.');
-      output.writeln();
-    } else {
-      output.writeln('<details>');
-      output.writeln('<summary>${subsection.value.length} commits...</summary>');
-      output.writeln();
-    }
+    var first = true;
+    for (final subsection in commits.entries) {
+      _output.writeln('### ${subsection.key}');
+      _output.writeln();
 
-    for (var commit in subsection.value) {
-      final pullRequest = commit.pullRequest;
-      final commitedAt = DateFormat.yMMMMd().format(commit.commitDate);
-      final reviewDuration = commit.commitDate.difference(pullRequest.createdAt);
-
-      final reviewers = pullRequest.reviews
-          .map((r) => '[${r.reviewerName ?? r.reviewerLogin}](${r.reviewerUrl})')
-          .join(', ');
-
-      final images = <String>{
-        for (final match in _htmlImageRegex.allMatches(pullRequest.body))
-          match.group(0)!,
-
-        for (final match in _mdImageRegex.allMatches(pullRequest.body))
-          match.group(0)!,
-      };
-
-      output.write('* ');
-      output.write('**[${pullRequest.authorName ?? pullRequest.authorLogin}](${pullRequest.authorUrl})** ');
-      output.write('&mdash; ');
-      output.write(pullRequest.title);
-      output.write(htmlBreak);
-      output.writeln();
-
-      output.write('    ');
-      output.write('<sub>');
-      output.write('[#${pullRequest.number}](${pullRequest.url}) merged on $commitedAt ');
-      output.write('&mdash; ');
-      output.write('**${_size(pullRequest)}:** ');
-      output.write('[${_pluralize(pullRequest.comments, 'comment')}](${pullRequest.url}) ');
-      output.write('over ${_humanizeDuration(reviewDuration)}, ');
-      output.write('${_pluralize(pullRequest.additions, 'addition')} and ');
-      output.write('${_pluralize(pullRequest.deletions, 'deletion')} in ');
-      output.write(_pluralize(pullRequest.changedFiles, 'file'));
-      output.write('</sub>');
-      output.write(htmlBreak);
-      output.writeln();
-
-      output.write('    ');
-      output.write('<sub>');
-      output.write('Reviewed by: ');
-      output.write(reviewers);
-
-      final extraReviewers = pullRequest.totalReviews - pullRequest.reviews.length;
-      if (extraReviewers > 0) {
-
-        output.write(', and ');
-        output.write('[${_pluralize(extraReviewers, 'other')}](${pullRequest.url})');
+      if (first) {
+        _output.writeln('${subsection.value.length} commits.');
+        _output.writeln();
+      } else {
+        _output.writeln('<details>');
+        _output.writeln('<summary>${subsection.value.length} commits...</summary>');
+        _output.writeln();
       }
 
-      output.write('</sub>');
-      output.write(htmlBreak);
-      output.writeln();
+      for (var commit in subsection.value) {
+        final pullRequest = commit.pullRequest;
+        final commitedAt = DateFormat.yMMMMd().format(commit.commitDate);
+        final reviewDuration = commit.commitDate.difference(pullRequest.createdAt);
 
-      if (images.isNotEmpty) {
-        output.write('    ');
-        output.write('<sub>');
-        output.write('<details>');
-        output.write('<summary>${_pluralize(images.length, 'image')}...</summary>');
-        output.writeAll(images);
-        output.write('</details>');
-        output.write('</sub>');
-        output.writeln();
+        final reviewers = pullRequest.reviews
+            .where((r) => r.reviewerLogin != null)
+            .map((r) => '[${r.reviewerName ?? r.reviewerLogin}](${r.reviewerUrl})')
+            .join(', ');
+
+        final images = <String>{
+          for (final match in _htmlImageRegex.allMatches(pullRequest.body))
+            match.group(0)!,
+
+          for (final match in _mdImageRegex.allMatches(pullRequest.body))
+            match.group(0)!,
+        };
+
+        _output.write('* ');
+        _output.write('**[${pullRequest.authorName ?? pullRequest.authorLogin}](${pullRequest.authorUrl})** ');
+        _output.write('&mdash; ');
+        _output.write(pullRequest.title);
+        _output.write(htmlBreak);
+        _output.writeln();
+
+        _output.write('  ');
+        _output.write('<sub>');
+        _output.write('[#${pullRequest.number}](${pullRequest.url}) merged on $commitedAt ');
+        _output.write('&mdash; ');
+        _output.write('**${_size(pullRequest)}:** ');
+        _output.write('[${_pluralize(pullRequest.comments, 'comment')}](${pullRequest.url}) ');
+        _output.write('over ${_humanizeDuration(reviewDuration)}, ');
+        _output.write('${_pluralize(pullRequest.additions, 'addition')} and ');
+        _output.write('${_pluralize(pullRequest.deletions, 'deletion')} in ');
+        _output.write(_pluralize(pullRequest.changedFiles, 'file'));
+        _output.write('</sub>');
+        _output.write(htmlBreak);
+        _output.writeln();
+
+        _output.write('  ');
+        _output.write('<sub>');
+        _output.write('Reviewed by: ');
+        _output.write(reviewers);
+
+        final extraReviewers = pullRequest.totalReviews - pullRequest.reviews.length;
+        if (extraReviewers > 0) {
+          _output.write(', and ');
+          _output.write('[${_pluralize(extraReviewers, 'other')}](${pullRequest.url})');
+        }
+
+        _output.write('</sub>');
+        _output.write(htmlBreak);
+        _output.writeln();
+
+        if (images.isNotEmpty) {
+          _output.write('  ');
+          _output.write('<sub>');
+          _output.write('<details>');
+          _output.write('<summary>${_pluralize(images.length, 'image')}...</summary>');
+          _output.writeAll(images);
+          _output.write('</details>');
+          _output.write('</sub>');
+          _output.writeln();
+        }
+
+        _output.writeln();
       }
 
-      output.writeln();
-    }
+      if (!first) {
+        _output.writeln('</details>');
+        _output.writeln();
+      }
 
-    if (!first) {
-      output.writeln('</details>');
-      output.writeln();
+      first = false;
     }
+  }
 
-    first = false;
+  Future<void> dispose() async {
+    await _output.flush();
+    _output.close();
   }
 }
 
-void writeCommitsTable(
-  IOSink output,
-  String owner,
-  String repository,
-  Map<String, List<Commit>> commits
-) {
-  output.writeln('## $owner/$repository');
-  output.writeln();
+class UnassignedPullRequestWriter {
+  UnassignedPullRequestWriter(this._output);
 
-  var first = true;
-  for (final subsection in commits.entries) {
-    output.writeln('### ${subsection.key}');
-    output.writeln();
+  final IOSink _output;
 
-    if (first) {
-      output.writeln('${subsection.value.length} commits.');
-      output.writeln();
-    } else {
-      output.writeln('<details>');
-      output.writeln('<summary>${subsection.value.length} commits...</summary>');
-      output.writeln();
-    }
-
-
-    output.writeln('Name | Author | Reviewers | Size');
-    output.writeln('-- | -- | -- | --');
-
-    for (var commit in subsection.value) {
-      final pullRequest = commit.pullRequest;
-      final commitedAt = DateFormat.yMMMMd().format(commit.commitDate);
-      final reviewDuration = commit.commitDate.difference(pullRequest.createdAt);
-
-      final reviewers = pullRequest.reviews
-          .map((r) => '[${r.reviewerLogin}](${r.reviewerUrl})')
-          .join('<br />');
-
-      output.writeln(
-        '[${pullRequest.title}](${pullRequest.url})'
-        '<br />'
-        '<sub>'
-          '[#${pullRequest.number}](${pullRequest.url}) merged on $commitedAt <br /> '
-          '[${_pluralize(pullRequest.comments, 'comment')}](${pullRequest.url}) over ${_humanizeDuration(reviewDuration)}'
-        '</sub>'
-        ' | '
-        '[${pullRequest.authorLogin}](${pullRequest.authorUrl})'
-        ' | '
-        '$reviewers'
-        ' | '
-        '<div title="'
-          '${_pluralize(pullRequest.additions, 'addition')} and '
-          '${_pluralize(pullRequest.deletions, 'deletion')} in '
-          '${_pluralize(pullRequest.changedFiles, 'file')}'
-        '">'
-        '${_size(pullRequest, compact: true)}'
-        '</div>'
-      );
-    }
-
-    output.writeln();
-
-    if (!first) {
-      output.writeln('</details>');
-      output.writeln();
-    }
-
-    first = false;
+  void writeHeader() {
+    _output.writeln('# Flutter pull requests without reviewers');
+    _output.writeln();
   }
-}
 
-void writePullRequests(
-  IOSink output,
-  String owner,
-  String repository,
-  List<PullRequest> pullRequests,
-) {
-  output.writeln('## $owner/$repository');
-  output.writeln();
+  void writePullRequests(
+    String owner,
+    String repository,
+    List<PullRequest> pullRequests,
+  ) {
+    _output.writeln('## $owner/$repository');
+    _output.writeln();
 
-  for (final pullRequest in pullRequests) {
-    final createdAt = DateFormat.yMMMMd().format(pullRequest.createdAt);
+    for (final pullRequest in pullRequests) {
+      final createdAt = DateFormat.yMMMMd().format(pullRequest.createdAt);
 
-    output.write('* ');
-    output.write('**[${pullRequest.authorName ?? pullRequest.authorLogin}](${pullRequest.authorUrl})** ');
-    output.write('&mdash; ');
-    output.write(pullRequest.title);
-    output.write('<br />');
-    output.writeln();
+      _output.write('* ');
+      _output.write('**[${pullRequest.authorName ?? pullRequest.authorLogin}](${pullRequest.authorUrl})** ');
+      _output.write('&mdash; ');
+      _output.write(pullRequest.title);
+      _output.write('<br />');
+      _output.writeln();
 
-    output.write('    ');
-    output.write('<sub>');
-    output.write('[#${pullRequest.number}](${pullRequest.url}) opened on on $createdAt ');
-    output.write('&mdash; ');
-    output.write('**${_size(pullRequest)}:** ');
-    output.write('[${_pluralize(pullRequest.comments, 'comment')}](${pullRequest.url}) ');
-    output.write('${_pluralize(pullRequest.additions, 'addition')} and ');
-    output.write('${_pluralize(pullRequest.deletions, 'deletion')} in ');
-    output.write(_pluralize(pullRequest.changedFiles, 'file'));
-    output.write('</sub>');
-    output.write('<br />');
-    output.writeln();
-    output.writeln();
+      _output.write('  ');
+      _output.write('<sub>');
+      _output.write('[#${pullRequest.number}](${pullRequest.url}) opened on on $createdAt ');
+      _output.write('&mdash; ');
+      _output.write('**${_size(pullRequest)}:** ');
+      _output.write('[${_pluralize(pullRequest.comments, 'comment')}](${pullRequest.url}) ');
+      _output.write('${_pluralize(pullRequest.additions, 'addition')} and ');
+      _output.write('${_pluralize(pullRequest.deletions, 'deletion')} in ');
+      _output.write(_pluralize(pullRequest.changedFiles, 'file'));
+      _output.write('</sub>');
+      _output.write('<br />');
+      _output.writeln();
+      _output.writeln();
+    }
+  }
+
+  Future<void> dispose() async {
+    await _output.flush();
+    _output.close();
   }
 }
 
